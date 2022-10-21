dc22a0fc5b6f9582c1ced5288080d5556d987703
Margaret Kenda <margaret.kenda@yahoo.com>
2022-10-21 08:07:46 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
+book1= Book.new("And Then There Were None")
+book1.author = "Agatha Christie"
+book1.page_count = 272
+book1.genre = "Mystery"
